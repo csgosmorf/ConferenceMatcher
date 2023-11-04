4074d3e7861aff2eba27bc467cbda962ef969500
csgosmorf <csgosmorf@gmail.com>
2023-11-03 17:05:04 -0700

    changed sizing for mobile

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setHTML(item, msg) {
     item.innerHTML = msg
 }
 
-createElement('h2', "Filter by Practice Location")
+createElement('h2', "Filter by Practice Location XYZ")
 
 
 stateNames = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
